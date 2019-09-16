09bed992c5a53b3780ebb89489d1779d92b3448a
Javier Santiago Barbosa Castillo <js.barbosa11@uniandes.edu.co>
2019-09-16 16:59:35 -0500

    build

---
+++ front/build/precache-manifest.afe93c293fb7e5362d20fb97a8cedf91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65357ba27d79a8375bba9ad9cca7fd09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9909edad48e004b843c0",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "516f974f493561d326e6",
+    "url": "/static/js/2.73c2a480.chunk.js"
+  },
+  {
+    "revision": "9909edad48e004b843c0",
+    "url": "/static/js/main.ba250d08.chunk.js"
+  },
+  {
+    "revision": "9ba826fd182ef2cb5489",
+    "url": "/static/js/runtime~main.cef8c144.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
